c2384bb0249dac11b823e8821c263013c1377608
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2022-11-13 12:36:28 -0600

    Update model.py

--- model.py
+++ model.py
@@ -58,3 +58,83 @@ model = tf.keras.models.Sequential([
 #Print model details
 
 model.summary()
+
+#Import RMSprop from keras
+
+from tensorflow.keras.optimizers import RMSprop
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(learning_rate=0.001),
+              metrics=['accuracy'])
+
+#Image generator with database
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale=1/255)
+validation_datagen = ImageDataGenerator(rescale=1/255)
+
+train_generator = train_datagen.flow_from_directory(
+        './novirus-or-virus/', 
+        target_size=(500, 500),
+        batch_size=32,
+        class_mode='binary') 
+
+validation_generator = validation_datagen.flow_from_directory(
+        './validation-novirus-or-virus/', 
+        target_size=(500, 500), 
+        batch_size=16,
+        class_mode='binary')
+
+history = model.fit(
+      train_generator,
+      steps_per_epoch=3,  
+      epochs=5,
+      verbose=1,
+      validation_data = validation_generator,
+      validation_steps=4)
+
+model_json = model.to_json()
+with open("model.json","w") as json_file:
+  json_file.write(model_json)
+  model.save_weights("m.h5")
+    
+#Run model
+
+import numpy as np
+from google.colab import files
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+uploaded = files.upload()
+
+for fn in uploaded.keys():
+ 
+  # predicting images
+  path = '/content/' + fn
+  img = tf.keras.utils.load_img(path, target_size=(500, 500))
+  x = tf.keras.utils.img_to_array(img)
+  x /= 50
+  x = np.expand_dims(x, axis=0)
+
+  images = np.vstack([x])
+  classes = model.predict(images, batch_size=32)
+  print(classes[0])
+    
+  if classes[0]>0.08:
+    img = mpimg.imread(path)
+    imgplot = plt.imshow(img)
+    plt.show()
+
+    print("the file" + fn + " contains virus")
+    print()
+  else:
+    img = mpimg.imread(path)
+    imgplot = plt.imshow(img)
+    plt.show()
+
+    print("the file" + fn + " does not contains virus")
+    print()
+
